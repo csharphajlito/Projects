31ebb500cb5284bea690ed52c603680462d4e61d
Stier Máté <stierMateGy@gmaiil.com>
2021-04-14 15:09:14 +0200

    tet

---
+++ Sudoku_Solution/Sudoku/SudokuEventargs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    public class SudokuEventargs :EventArgs
+    {
+        public bool isBacktrack { get; set; }
+        public int ActualPosition { get; }
+        public List<int> FieldValues { get; set; }
+        public List<int> WrongNumbers { get; set; }
+
+        public SudokuEventargs(List<int> wrongNumbers,int actualPosition)
+        {
+            WrongNumbers = wrongNumbers;
+            ActualPosition = actualPosition;
+            isBacktrack = false;
+        }
+    }
+}
